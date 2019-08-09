1fc15253c5503570d5697405a414980e4297b48b
Brian Balderston <brian.j.balderston.civ@mail.mil>
2019-08-09 09:15:45 -0400

    Check that client cert was issued by root CA

--- CACDemoNetCore3/MyCertificateValidationService.cs
+++ CACDemoNetCore3/MyCertificateValidationService.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CACDemoNetCore3
 {
@@ -6,7 +7,16 @@ namespace CACDemoNetCore3
     {
         public bool ValidateCertificate(X509Certificate2 clientCertificate)
         {
-            return clientCertificate.Issuer.StartsWith("CN=DOD ID CA"); //TODO: Get DoD root cert and validate chain
+            var rootCert = new X509Certificate2("rootCA.cer");
+
+            var clientChain = new X509Chain();
+            clientChain.Build(clientCertificate);
+
+            var clientChainElements = clientChain.ChainElements;
+
+            var clientRootCert = clientChainElements[clientChainElements.Count - 1].Certificate;
+
+            return rootCert.Thumbprint == clientRootCert.Thumbprint;
         }
     }
 }
